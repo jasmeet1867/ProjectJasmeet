16c25e77043dc0d46c77e3bab363274a57b806cd
Jasmeet Singh <jasmeet1867@gmail.com>
2026-06-01 12:52:11 -0400

    Added initial setup feature

--- src/main/java/com/mycompany/projectjasmeet/ProjectJasmeet.java
+++ src/main/java/com/mycompany/projectjasmeet/ProjectJasmeet.java
@@ -10,7 +10,8 @@ package com.mycompany.projectjasmeet;
  */
 public class ProjectJasmeet {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println("Checking if feature branch work");
+}
 }
